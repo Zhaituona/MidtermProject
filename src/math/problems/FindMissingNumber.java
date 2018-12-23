bf2c1473477aa5e6c8d674a793bcc9359b1c8a3d
Zhaituona <zaytuna128@gmail.com>
2018-12-23 14:54:34 -0500

    added math problems

--- src/math/problems/FindMissingNumber.java
+++ src/math/problems/FindMissingNumber.java
@@ -5,6 +5,29 @@ package math.problems;
  */
 public class FindMissingNumber {
 
+    // this is another way create method
+
+    public static int findMissingNumber(int[] array, int n) {
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {// take the length ,
+            sum = sum + array[i];
+        }
+        System.out.println(sum);
+
+        int total = 0;// have to take all ,cause one is missing
+        for (int j = 0; j <= 10; j++) {// it will take all of them
+            //(int j =-1; j<=10; j++)// the smallest one start from -1 ,
+            total = total + j;
+        }
+        return total-sum;
+       // System.out.println("******************);
+       // System.out.println(total);
+       // System.out.println("Missing number is : " + (total - sum));
+    }
+
+
+
 
 
     public static void main(String[] args) {
--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -1,8 +1,33 @@
 package math.problems;
+import org.testng.Assert;
+
 
 public class UnitTestingMath {
     public static void main(String[] args) {
         //Apply Unit testing into each classes and methods in this package.
+
+        // testing for factorial
+        Assert.assertEquals(120,Factorial.factorial(5));// this for factorial
+
+        // testing for find lowest difference
+       int [] array1 = {12,36,3,5,7,8,90};
+       int[] array2 = {11,98,6,45,2,1,3,76,1};
+       Assert.assertEquals(1,FindLowestDifference.lowestDiff(array1,array2));
+
+       // testing for find missing number
+       int [] array3 = {1,2,3,4,5,6,8,9,10};
+       Assert.assertEquals(7,FindMissingNumber.findMissingNumber(array3,10));
+
+       // testing for find lowest number
+       int [] array4 = {1,36,2,5,8,9,78};
+       Assert.assertEquals(1,LowestNumber.findLowestNumber(array4));
+
+
+
+
+
+
+
 
 
 
